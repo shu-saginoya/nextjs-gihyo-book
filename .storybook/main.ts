a24d86f710e39d8880f19a35c1ecd875b19be27b
shu.saginoya <shu.saginoya@gmail.com>
2024-11-20 16:10:21 +0900

    Update StoryBook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -7,10 +9,34 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
+  },
+  staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-propery-in-object",
+    ],
+  }),
+  webpackFinal: async (config) => {
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+    if (!config.resolve.plugins) {
+      config.resolve.plugins = [];
+    }
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ];
+    return config;
   },
 };
 export default config;
