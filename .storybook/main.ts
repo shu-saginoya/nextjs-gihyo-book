a6342c87543e4a16aa9cc350e7e90a4d0c02b4d8
shu.saginoya <shu.saginoya@gmail.com>
2024-12-10 00:40:33 +0900

    Update Tailwindcss config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,42 +1,48 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
-import path from "path";
+import type { StorybookConfig } from '@storybook/nextjs'
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
+import path from 'path'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/addon-postcss",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["public"],
+  staticDirs: ['public'],
   babel: async (options) => ({
     ...options,
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-private-methods",
-      "@babel/plugin-proposal-private-propery-in-object",
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-propery-in-object',
     ],
   }),
   webpackFinal: async (config) => {
     if (!config.resolve) {
-      config.resolve = {};
+      config.resolve = {}
     }
     if (!config.resolve.plugins) {
-      config.resolve.plugins = [];
+      config.resolve.plugins = []
     }
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../tsconfig.json"),
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
       }),
-    ];
-    return config;
+    ]
+    return config
   },
-};
-export default config;
+}
+export default config
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#fe9611',
+        primaryDark: '#ef7b07',
+        secondary: '#3577fc',
+        secondaryDark: '#1741de',
+        danger: '#bf6e6e',
+        dangerDark: '#8f4646',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
