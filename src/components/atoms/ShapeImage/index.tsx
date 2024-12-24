e32040bd62bf40e145a96ffb7976bcf642785256
shu.saginoya <shu.saginoya@gmail.com>
2024-12-24 09:32:25 +0900

    Add atoms/ShapeImage/index.tsx

---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import React from 'react'
+
+type ImageShape = 'circle' | 'square'
+type ShapeImageProps = {
+  src: string
+  width: string
+  height: string
+  alt: string
+  shape?: ImageShape
+}
+
+const variants = {
+  // 円形
+  circle: `rounded-full`,
+  // 四角形
+  square: `rounded-none`,
+}
+
+/**
+ * シェイプイメージ
+ */
+const ShapeImage: React.FC<ShapeImageProps> = ({
+  src,
+  width,
+  height,
+  alt,
+  shape = 'circle',
+}) => {
+  const style = variants[shape]
+
+  return (
+    <Image
+      src={src}
+      width={Number(width)}
+      height={Number(height)}
+      alt={alt}
+      className={style}
+    />
+  )
+}
+
+export default ShapeImage
